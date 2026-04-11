0c20499bbce94570f28e203b6e03c1b3cd5f438d
praghosh <srijaneeghosh1@gmail.com>
2026-04-12 04:18:20 +0530

    gap decrease

--- frontend/src/components/Stats.jsx
+++ frontend/src/components/Stats.jsx
@@ -24,7 +24,7 @@ const dbKeyMap = {
 
 const Stats = ({ dynamicStats }) => {
   return (
-    <section className="py-16 lg:py-20 bg-slate-50">
+    <section className="pt-4 pb-16 lg:py-20 bg-slate-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         
         {/* Section Header */}
